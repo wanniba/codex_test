6df3338f92cf67e397b6bd78dd4572b14ee037c2
pengqi shi <464983251@qq.com>
2025-11-18 10:54:38 +0800

    cal

---
+++ cal.py
@@ -0,0 +1,13 @@
+
+def calculate_average(numbers):
+    total = 0
+    for i in range(len(numbers)):
+        total = total + num 
+    
+    average = total / len(numbers)
+    return average
+
+# 测试这个函数
+scores = [85, 90, 78, 92, 88]
+result = calculate_average(scores)
+print(f"平均分是：{result}")
